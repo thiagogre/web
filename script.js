e33cff7a27d2620611d5ca3df5ad031ad312c1fa
thiago <thiagoluiz_16@hotmail.com>
2020-09-29 11:03:44 -0300

    refactor: remove spaces

--- script.js
+++ script.js
@@ -3,24 +3,24 @@
 const filterButtons=document.querySelector("#filter-btns").children;
 const items=document.querySelector(".hobbies-gallery").children;
 
-    for(let i=0; i<filterButtons.length; i++){
-        filterButtons[i].addEventListener("click", function(){
-            for(let j=0; j<filterButtons.length; j++){
-                filterButtons[j].classList.remove("active")
+for(let i=0; i<filterButtons.length; i++){
+    filterButtons[i].addEventListener("click", function(){
+        for(let j=0; j<filterButtons.length; j++){
+            filterButtons[j].classList.remove("active")
+        }
+        this.classList.add("active");
+        const target=this.getAttribute("data-target")
+        for(let k=0; k<items.length; k++){
+            items[k].style.display="none";
+            if(target==items[k].getAttribute("data-id")){
+                items[k].style.display="block";
             }
-            this.classList.add("active");
-            const target=this.getAttribute("data-target")
-            for(let k=0; k<items.length; k++){
-                items[k].style.display="none";
-                if(target==items[k].getAttribute("data-id")){
-                    items[k].style.display="block";
-                }
-                if(target=="all"){
-                    items[k].style.display="block";
-                }
+            if(target=="all"){
+                items[k].style.display="block";
             }
-        })
-    }
+        }
+    })
+}
 
 
     /* FIXING HEADER */
